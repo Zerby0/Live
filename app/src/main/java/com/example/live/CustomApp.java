f85aadc3e46c7afa1dd39100801b504f6894f507
Zerby <zerbi2002@gmail.com>
2024-05-25 16:53:29 +0200

    Settings LogRocket

---
+++ app/src/main/java/com/example/live/CustomApp.java
@@ -0,0 +1,21 @@
+package com.example.live;
+
+import android.app.Application;
+import android.content.Context;
+import com.logrocket.core.SDK;
+
+public class CustomApp extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        SDK.init(
+                this,
+                base,
+                options -> {
+                    options.setAppID("zerbys/live");
+                }
+        );
+    }
+}
--- app/src/main/java/com/example/live/MainActivity.kt
+++ app/src/main/java/com/example/live/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.live
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import com.logrocket.core.SDK
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.v(ContentValues.TAG, "Si sta avviando l'app")
+        val userData: MutableMap<String, String> = HashMap()
+
+        userData["name"] = "Zerby"
+        userData["email"] = "sonoGay@gmail.com"
+        userData["subscriptionPlan"] = "premium"
+
+        SDK.identify("28dvm2jfa", userData)
+        Log.v(ContentValues.TAG, "Identity funziona")
 
     }
 }
