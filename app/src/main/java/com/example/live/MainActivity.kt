6c2aa24eaadf325c5e4c1a65b5153128890049c8
simionRiccard0 <riccardo.simion@studenti.unipd.it>
2024-05-25 16:26:31 +0200

    fixato styles.xml in modo che la app non esploda più all'apertura (java.lang.IllegalStateException: You need to use a Theme.AppCompat theme (or descendant) with this activity.).

--- app/src/main/java/com/example/live/MainActivity.kt
+++ app/src/main/java/com/example/live/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.live
 
-import android.content.ContentValues
+import  android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.logrocket.core.SDK
-
-
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
         Log.v(ContentValues.TAG, "Si sta avviando l'app")
         val userData: MutableMap<String, String> = HashMap()
 
         userData["name"] = "Zerby"
-        userData["email"] = "sonoGay@gmail.com"
+        userData["email"] = "trial@gmail.com"
         userData["subscriptionPlan"] = "premium"
 
         SDK.identify("28dvm2jfa", userData)
