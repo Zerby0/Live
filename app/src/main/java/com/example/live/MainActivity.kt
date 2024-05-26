117100e1724895a81781818c88608f31b8e2636c
simionRiccard0 <riccardo.simion@studenti.unipd.it>
2024-05-26 09:55:17 +0200

    create activity per dati biometrici e statistiche settimanali, navigazione tramite intent. Da implementare un UI migliore graficamente per tutte le activity. Rendere dinamico l'aggiornamento dei valori di passi e calorie

--- app/src/main/java/com/example/live/MainActivity.kt
+++ app/src/main/java/com/example/live/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.logrocket.core.SDK
+import android.content.Intent
+import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,13 +15,23 @@ class MainActivity : AppCompatActivity() {
         Log.v(ContentValues.TAG, "Si sta avviando l'app")
         val userData: MutableMap<String, String> = HashMap()
 
-        userData["name"] = "Zerby"
+        userData["name"] = "Sim"
         userData["email"] = "trial@gmail.com"
         userData["subscriptionPlan"] = "premium"
 
         SDK.identify("28dvm2jfa", userData)
         Log.v(ContentValues.TAG, "Identity funziona")
 
+        val statistics = findViewById<Button>(R.id.statistiche_settimanali_button)
+        statistics.setOnClickListener {
+            val myIntent = Intent(it.context, StatisticsActivity::class.java)
+            startActivity(myIntent)
+        }
+        val biometrics = findViewById<Button>(R.id.dati_biometrici_button)
+        biometrics.setOnClickListener {
+            val myIntent = Intent(it.context, BiometricsActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
 
---
+++ app/src/main/java/com/example/live/StatisticsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.live
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+class StatisticsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistics)
+
+        val previous : Button = findViewById(R.id.back_sender_button)
+        previous.setOnClickListener {
+            finish()
+        }
+
+    }
+}
