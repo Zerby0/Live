c26d1be9a3155993fcb8b6456b3a1b0be6fbd1ab
simionRiccard0 <riccardo.simion@studenti.unipd.it>
2024-05-31 16:24:56 +0200

    fix sign in, implementate funz bio e ui bio

--- app/src/main/java/com/example/live/BiometricsFragment.kt
+++ app/src/main/java/com/example/live/BiometricsFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.live.databinding.FragmentBiometricBinding
-import com.example.live.databinding.FragmentHomeBinding
-import com.example.live.databinding.FragmentStatisticBinding
 
 class BiometricsFragment : Fragment() {
 
@@ -24,13 +22,49 @@ class BiometricsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // Puoi aggiungere logica per il tuo fragment qui
-        binding.textBiometrics.text = "Welcome to Biometrics Fragment"
+        binding.btnCalculate.setOnClickListener{
+            calculateBiometrics()
+        }
     }
+    private fun calculateBiometrics() {
+        val selectedSexId = binding.radioGroupSex.checkedRadioButtonId
+        val sex = if (selectedSexId == R.id.radioMale) "M" else "F"
+        val age = binding.editTextText.text.toString().toIntOrNull()
+        val height = binding.editTextText2.text.toString().toIntOrNull()
+        val weight = binding.editTextText3.text.toString().toIntOrNull()
 
+        if (age == null || height == null || weight == null) {
+            binding.tvResults.text = "Per favore, inserisci tutti i campi correttamente."
+            return
+        }
+
+        val bmi = weight / ((height / 100.0) * (height / 100.0))
+        val bmr = if (sex == "M") {
+            88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+        } else {
+            447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
+        }
+        val tdee = bmr * 1.55  // assuming moderate activity level
+
+        val bodyFatPercentage = if (sex == "M") {
+            1.20 * bmi + 0.23 * age - 16.2
+        } else {
+            1.20 * bmi + 0.23 * age - 5.4
+        }
+        val leanBodyMass = weight * (1 - bodyFatPercentage / 100)
+        val fatMass = weight - leanBodyMass
+
+        binding.tvResults.text = """
+            Indice di Massa Corporea (IMC): ${String.format("%.2f", bmi)}
+            Massa Magra: ${String.format("%.2f", leanBodyMass)} kg
+            Massa Grassa: ${String.format("%.2f", fatMass)} kg
+            Fabbisogno Energetico: ${String.format("%.2f", tdee)} kcal
+        """.trimIndent()
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
